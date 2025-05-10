f9055ed7bde7042b41b649dccc63c7511b9f03da
robbopp <robert.oprean10@gmail.com>
2025-05-10 15:34:38 +0300

    Add DiceView

---
+++ DiceRoller/DIceView.swift
@@ -0,0 +1,18 @@
+//
+//  DIceView.swift
+//  DiceRoller
+//
+//  Created by Robert Oprean on 10.05.2025.
+//
+
+import SwiftUI
+
+struct DIceView: View {
+    var body: some View {
+        Image(systemName: "die.face.1")
+    }
+}
+
+#Preview {
+    DIceView()
+}
