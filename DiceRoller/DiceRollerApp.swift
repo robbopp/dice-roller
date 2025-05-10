15c843cce35b90eb726e9ed831c426e278e69c97
robbopp <robert.oprean10@gmail.com>
2025-05-10 15:17:23 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Robert Oprean on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
